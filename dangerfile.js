d8f76d966e4f642bcee58e48396981ffb579367e
Adrian Ferreres Esteller <ardiadrianadri@gmail.com>
2019-04-03 15:47:32 +0200

    The PR must have a description

--- dangerfile.js
+++ dangerfile.js
@@ -3,5 +3,10 @@ import {message, danger, fail} from "danger"
 if (!danger.github.pr.assignee) {
   fail('This pull request needs an assignee and optionally include any reviewers')
 }
+
+if (danger.github.pr.body.length === 0) {
+  fail('The pull request should have a description')
+}
+
 const modifiedMD = danger.git.modified_files.join("- ")
 message("Changed Files in this PR: \n - " + modifiedMD)
