2793e34b2228760fa0aecb43b5d76aa8f5fc91eb
Adrian Ferreres Esteller <ardiadrianadri@gmail.com>
2019-04-03 14:54:17 +0200

    Add check if the PR has a person assigned

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,7 @@
-import {message, danger} from "danger"
+import {message, danger, fail} from "danger"
 
+if (!danger.github.pr.assignee) {
+  fail('This pull request needs an assignee and optionally include any reviewers')
+}
 const modifiedMD = danger.git.modified_files.join("- ")
 message("Changed Files in this PR: \n - " + modifiedMD)
