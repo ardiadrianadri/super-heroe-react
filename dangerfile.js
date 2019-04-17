b54aa896b028666c6fbfbe570ca4ae039c9904b1
Adrian Ferreres <ardiadrianadri@gmail.com>
2019-04-17 07:52:13 +0200

    Add branch name validation

--- dangerfile.js
+++ dangerfile.js
@@ -1,12 +1,59 @@
+/* eslint-disable no-undef */
 import {message, danger, fail} from "danger"
 
+const owner = process.env.GITHUB_OWNER || 'ardiadrianadri';
+const repoName = process.env.GITHUB_REPO || 'super-heroe-react';
+const fails = []
+const validBranchName = /^(feature|bugfix|refactor|hotfix)\/.*$/g;
+const leng = danger.github.commits.length;
+const lastCommit = danger.github.commits[leng-1];
+
+function finalJudgment (fails) {
+  const leng = fails.length;
+  
+  let msg = ''
+  if(leng > 0) {
+    msg += `This pull request is not worth for a superior race: \n`;
+    for (error of fails) {
+      msg += `- ${error} \n`
+    }
+    fail(msg)
+  } else {
+    message(`Congrats this pull request is a proud for you and all your race`)
+  }
+}
+
+message(`
+Hello:
+I am DevVox3000, the bot that came from the future to make sure that your pull request has the standard of
+a superior race.
+I'm going to inspect your work to make sure your RP has what it needs to have ... so be a good developer and wait quietly while you watch as the expert works.
+`)
+
 if (!danger.github.pr.assignee) {
-  fail('This pull request needs an assignee and optionally include any reviewers')
+  fails.push('The pull request must have, at least, an assignee... and some reviewer');
 }
 
 if (danger.github.pr.body.length === 0) {
-  fail('The pull request should have a description')
+  fails.push('This pull request deserves some description to be clear, Don\'t you think?')
 }
 
-const modifiedMD = danger.git.modified_files.join("- ")
-message("Changed Files in this PR: \n - " + modifiedMD)
+danger.github.api.request(
+  `/repos/${owner}/${repoName}/commits/${lastCommit.sha}/branches-where-head`,
+  {
+    headers: {
+      'accept': 'application/vnd.github.groot-preview+json'
+    }
+  }
+)
+.then(response => { 
+  const branches = response.data;
+  const branch = branches[branches.length - 1];
+  
+  if(!validBranchName.test(branch.name)) {
+    fails.push('I don\'t understand the reason to be of this PR. Have you read the rules to name the branches of the PR?')
+  }
+  
+  finalJudgment(fails)
+})
+.catch(error => { throw error })
