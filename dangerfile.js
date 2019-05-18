51a0bac29dc0831ced2ab66139433fb108aaed4c
Adrian Ferreres <adrian.ferreres@sngular.com>
2019-05-18 11:07:21 +0200

    Check reviewers

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,12 @@
-import {message, danger} from "danger"
+import {message, danger, fail} from "danger"
 
-const modifiedMD = danger.git.modified_files.join("- ")
-message("Changed Files in this PR: \n - " + modifiedMD)
+message(`
+Hola a todos soy devVox 3000 un bot venido del futuro (se que cuesta de creer pero realmente no existo)
+que esta aqui mismo para asegurse de que tus cambios en el código son dignos de una raza superior
+`);
+
+const reviewers = danger.github.requested_reviewers.users;
+
+if (reviewers.length === 0 ) {
+    fail('Pero bueno!!! te crees que puedes colar un cambio en mi guardia sin revisores y que no me de cuenta?');
+}
